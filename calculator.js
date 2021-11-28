06012e4ff3162158e6f3d44c6ba30363281ef7d2
jonahsaltzman <jonahsaltzman@gmail.com>
2021-11-27 19:33:16 -0500

    parser works except on operators; scope issue

--- calculator.js
+++ calculator.js
@@ -1,79 +1,59 @@
 'use strict'
 
-const {Stack, Queue} = require('classes.js')
+const {Stack, Queue} = require('./classes')
+
+const spaceRegEx = /\ /g
+const digitRegEx = /\d/
+const floatRegEx = /\d+\.\d+|\.\d+|\d+/
+const operatorRE = /[\(\)\+\-\*\/]/
 
 let calculator = {
-	add: (a, b) => {
-		let result = undefined
-		if (b !== undefined) {
-			result = a + b
-			calculator.value = result
-			return result
-		}
-		result = calculator.value + a
-		calculator.value = result
-		return result
-	},
-	subtract: (a, b) => {
-		let result = undefined
-		if (b !== undefined) {
-			result = a - b
-			calculator.value = result
-			return result
-		}
-		result = calculator.value - a
-		calculator.value = result
-		return result
-	},
-	multiply: (a, b) => {
-		let result = undefined
-		if (b !== undefined) {
-			result = a * b
-			calculator.value = result
-			return result
-		}
-		result = calculator.value * a
-		calculator.value = result
-		return result
-	},
-	divide: (a, b) => {
-		let result = undefined
-		if (b !== undefined) {
-			result = a / b
-			calculator.value = result
-			return result
-		}
-		result = calculator.value / a
-		calculator.value = result
-		return result
-	},
-	raise: (a, b) => {
-		let result = undefined
-		if (b !== undefined) {
-			result = Math.pow(a, b)
-			calculator.value = result
-			return result
-		}
-		result = Math.pow(calculator.value, a)
-		calculator.value = result
-		return result
-	},
-	value: 0,
-	clear: () => (calculator.value = 0),
+	queue: new Queue(),
+	stack: new Stack(),
+	tokens: [],
+	string: '',
     master: (string) => {
-        const reduced = string.replace(/ /g, '')
-        const output = []
-		const operators = []
-		for (let i = 0; i < reduced.length; i++) {
-
+		console.log(`parsing: `, string)
+		calculator.string = string.replace(spaceRegEx, '')
+        while (calculator.string) {
+			const char = calculator.string[0]
+			if (char.match(digitRegEx)) {
+				calculator.getDigits()
+			}
+			else if (char.match(operatorRE)) {
+				const noError = calculator.stack.push(char)
+				if (!noError) {
+					console.log(`there was an error pushing an operator to the stack!`)
+					return false
+				}
+				calculator.string = calculator.string.slice(1)
+			}
+		}
+		while (calculator.stack.peek()) {
+			if (calculator.stack.peek() === '(') {
+				console.log('after first loop, there is a mismatched parens!')
+				return false
+			}
+			calculator.queue.enqueue(calculator.stack.pop())
 		}
     },
-	isDigit: (n) => {}
+	getDigits: () => {
+		const digits = calculator.string.match(floatRegEx)
+		if (digits) {
+			calculator.string = calculator.string.slice(digits[0].length + 1)
+			calculator.queue.enqueue(parseInt(digits[0]))
+			return parseInt(digits[0])
+		}
+		console.log('getDigits error; regex match object:')
+		console.log(digits)
+		return false
+	}
 }
 
-let queue = {
-	items: [],
-	enqueue: (item) => this.items.push(item)
-}
+// module.exports = {
+// 	calculator
+// }
 
-calculator.master('(2 - 1) * (5 ^ 2)')
+calculator.master('22 + 15 * (3 + 142)')
+console.log(calculator.queue.getQueue())
+console.log(calculator.string)
--- classes.js
+++ classes.js
@@ -1,18 +1,82 @@
 'use strict'
 
+// const {calculator} = require('./calculator')
+
+const opNotLeftParen = /[\)\+\-\*\/]/
+
+const operators = [
+	{
+		symbol: '^',
+		precedence: 4,
+		associativity: false,
+	},
+	{
+		symbol: '*',
+		precedence: 3,
+		associativity: true,
+	},
+	{
+		symbol: '/',
+		precedence: 3,
+		associativity: true,
+	},
+	{
+		symbol: '+',
+		precedence: 2,
+		associativity: true,
+	},
+	{
+		symbol: '-',
+		precedence: 2,
+		associativity: true,
+	},
+]
+
 class Stack {
 	constructor() {
 		this.items = []
 	}
 	isEmpty = () => this.items.length === 0
-	push = (item) => this.items.unshift(item)
-	pop = () => (this.isEmpty() ? false : this.items.shift())
-	peek = () => (this.isEmpty() ? false : this.items[0])
+	push = (operator) => {
+        if (operator === '(') {
+            this.items.unshift(operator)
+            return true
+        }
+        if (operator === ')') {
+            while(this.peek() !== '(') {
+                if (this.isEmpty()) {
+                    console.log("mismatched parentheses!")
+                    return false
+                }
+                calculator.queue.enqueue(this.pop())
+            }
+            if (this.peek() === '(') {
+                this.pop()
+                return true
+            }
+            else {
+                console.log('there should have been a left paren here!')
+                return false
+            }
+        }
+        while (this.needToPop(operator)) calculator.queue.enqueue(this.pop())
+        this.items.unshift(operator)
+        return true
+    }
+	pop = () => this.isEmpty() ? false : this.items.shift()
+	peek = () => this.isEmpty() ? false : this.items[0]
 	print = () => {
 		let str = '['
 		this.items.forEach((item) => (str += item.toString() + ', '))
 		return (str += ']')
 	}
+	getStack = () => this.items
+    needToPop = (operator) => {
+        if (this.isEmpty) return false
+        const peekObj = operators.find(op => op.symbol === this.peek())
+        const opObj = operators.find(op => op.symbol === operator)
+        return this.peek().match(opNotLeftParen) && (peekObj.precedence > opObj.precedence || (peekObj.precedence === opObj.precedence && opObj.associativity))
+    }
 }
 
 class Queue {
@@ -28,6 +92,7 @@ class Queue {
 		this.items.forEach((item) => (str += item.toString() + ', '))
 		return (str += ']')
 	}
+    getQueue = () => this.items
 }
 
 module.exports = {
